e5da3c2c7501380e4736f65c666d5dc75f316ab1
Olivier <olivier.bitter@netstream.ch>
2022-04-07 12:47:31 +0200

    change task background to red

--- src/components/Task.svelte
+++ src/components/Task.svelte
@@ -34,7 +34,13 @@
     <span class="checkbox-custom" on:click={ArchiveTask} aria-label={`archiveTask-${task.id}`}/>
   </label>
   <div class="title">
-    <input type="text" readonly value={task.title} placeholder="Input title" />
+        <input
+            type="text"
+            readonly
+            value={task.title}
+            placeholder="Input title"
+            style="background: red;"
+        />
   </div>
   <div class="actions">
     {#if task.state !== 'TASK_ARCHIVED'}
